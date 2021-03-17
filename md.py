d6f83105c1a8f0ec061c71ce70a6a9ddf9dda9c8
Patrick Winter <patrickwinter@posteo.ch>
2021-03-17 20:26:57 +0100

    Parse headings

--- md.py
+++ md.py
@@ -3,5 +3,30 @@ from typing import NamedTuple
 import re
 
 
+class Header(NamedTuple):
+    level: int
+    text: str
+
+
+def headers(lines):
+    line = lines.popleft()
+    match = re.search(r"^(#+)\s(.*)$", line)
+    level = len(match.group(1))
+    text = match.group(2).rstrip()
+    return Header(level, text)
+
+
+PATTERNS = {r"^#+\s.*$": headers}
+
+
 def md(text):
-    return []
+    lines = deque(text.split("\n"))
+    tokens = []
+    while lines:
+        head = lines[0]
+        for pattern, parser in PATTERNS.items():
+            if re.match(pattern, head):
+                parsed = parser(lines)
+                tokens.append(parsed)
+
+    return tokens
--- md_test.py
+++ md_test.py
@@ -1,16 +1,23 @@
-from md import md
+import pytest
+from md import md, headers, Header
+from collections import deque
+
+
+@pytest.mark.parametrize(
+    "text,expected",
+    [
+        ("# Heading 1", Header(1, "Heading 1")),
+        ("# 1994", Header(1, "1994")),
+        ("## Subheading 1", Header(2, "Subheading 1")),
+        ("##### Why so many titles?", Header(5, "Why so many titles?")),
+    ],
+)
+def test_headers(text, expected):
+    assert headers(deque([text])) == expected
 
 
 def test_md():
 
-    text = """
-# Heading 1
+    text = """# Heading 1 """
 
-This is some text with **bold** text. This text stretches across multiple lines,
-which is pretty cool.
-
-* It also has a list
-* With two elements
-"""
-
-    assert md(text) == []
+    assert md(text) == [Header(level=1, text="Heading 1")]
